0f14a8e97fa1407ed3eac85b9b997cb3613d6868
Isabelle Kuder <isabelle.kuder@gmail.com>
2025-08-11 16:28:48 +0100

    updated readme

--- update_readme.py
+++ update_readme.py
@@ -10,7 +10,7 @@ CARD_HEIGHT = 550
 BADGES_PADDING = 15
 CARD_BORDER_RADIUS = 15
 BLURB_PADDING = 30
-SCALE_FACTOR = 4
+SCALE_FACTOR = 6
 SHADOW_OFFSET = 20
 
 # --- Light & Dark Theme Styles ---
@@ -23,10 +23,10 @@ BOX_SHADOW_LIGHT = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0
 # Dark Theme
 CARD_BG_COLOR_DARK = "rgba(30,36,40, 1)"  # A dark grey
 TITLE_COLOR_DARK = "#f0f6fc"
-BLURB_COLOR_DARK = "#b7bbc0"
+BLURB_COLOR_DARK = "#d3d7dc"
 # A subtle white "glow" for the dark mode shadow
 BOX_SHADOW_DARK = (
-    "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(255, 255, 255, 0.1);"
+    "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 10px 20px 0 rgba(255, 255, 255, 0.1);"
 )
 
 # --- Font Customization ---
